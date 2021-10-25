4d7684a6371c3bd9dcb8a6e415d914ad3439e335
ryan waples <ryan.waples@gmail.com>
2021-10-25 13:45:40 -0700

    update gitignore, config, and install files

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,16 @@
 mkdir ./programs
 
+
+# BCFTOOLS
+cd programs
+wget https://github.com/samtools/bcftools/releases/download/1.14/bcftools-1.14.tar.bz2
+tar -xjvf bcftools-1.14.tar.bz2
+cd bcftools-1.14/
+./configure
+make
+cd ../..
+
+
 ## BEAGLE (v5.2 28Jun21)
 mkdir programs/BEAGLE
 cd programs/BEAGLE
