ae2f4d5c0fd27336554368c646bbafc43e0b7664
ryan waples <ryan.waples@gmail.com>
2021-11-09 14:45:35 -0800

    update env and R for MOSAIC, update instaallation guide

--- install_programs.sh
+++ install_programs.sh
@@ -17,6 +17,7 @@ cd programs/BEAGLE
 wget https://faculty.washington.edu/browning/beagle/beagle.28Jun21.220.jar
 cd ../..
 
+
 ## RFMIX version 1 (1.5.4)
 mkdir programs/RFmix
 cd programs/RFmix
@@ -28,6 +29,7 @@ g++ -Wall -O3 -ftree-vectorize -fopenmp main.cpp getdata.cpp randomforest.cpp cr
 cd ../TrioPhased/
 g++ -Wall -O3 -ftree-vectorize -fopenmp main.cpp getdata.cpp randomforest.cpp crfviterbi.cpp windowtosnp.cpp -o RFMix_TrioPhased
 cd ../../../..
+
 
 ## RFMIX version 2 (v2.03-r0)
 mkdir programs/RFmix2
@@ -46,32 +48,18 @@ wget https://www.haplotype.org/download/elai-latest.tar.gz
 tar -xvf ./elai-latest.tar.gz
 
 ## MOSAIC (https://maths.ucd.ie/~mst/MOSAIC/)
-## requires R (4?)
-wget https://maths.ucd.ie/~mst/MOSAIC/MOSAIC_1.3.8.tar.gz
-
+## requires R v4
 mkdir ./programs/MOSAIC
 cd ./programs/MOSAIC
-conda activate ./r-env
-wget https://csgitlab.ucd.ie/mst/mosaic/-/archive/master/mosaic-master.tar.gz
-mkdir ./r-packages
+wget https://maths.ucd.ie/~mst/MOSAIC/MOSAIC_1.3.9.tar.gz
+# within R
+#install.packages("MOSAIC_1.3.9.tar.gz")
+# back in shell
+git clone https://csgitlab.ucd.ie/mst/mosaic.git
 
-#R CMD INSTALL -l ./r-packages mosaic-master.tar.gz
 
-# from within R
-install.packages("doParallel")
-install.packages("Rcpp")
-install.packages("ff")
-install.packages("combinat")
-install.packages("LaF")
-install.packages("argparser")
-install.packages("mosaic-master.tar.gz")
-
-# done with R
-tar -xvf mosaic-master.tar.gz
-cd mosaic-master/
-
-# MOSAIC
-cd ./programs/MOSAIC/MOSAIC
+# MOSAIC example
+cd ./programs/MOSAIC/mosaic
 Rscript mosaic.R Moroccan example_data/ -a 2 -n 2 -c 18:22
 cd ../../..
 
@@ -81,11 +69,3 @@ cd ../../..
 cd ./programs/RFmix/RFMix_v1.5.4
 python2 RunRFMix.py TrioPhased ./TestData/alleles1.txt ./TestData/classes.txt ./TestData/markerLocationsChr1.txt -o outputTrioPhased
 cd ../../..
-
-
-## BEAGLE
-java -jar programs/BEAGLE/beagle.29May21.d6d.jar \
-gt=results/local_ancestry/AmericanAdmixture_4B11/AA_42/test_anal_1.genotypes.target_inds.vcf.gz \
-ref=results/local_ancestry/AmericanAdmixture_4B11/AA_42/test_anal_1.genotypes.reference_inds.vcf.gz \
-map=results/local_ancestry/AmericanAdmixture_4B11/AA_42/test_anal_1.genetic_map.txt \
-out=~/temp
